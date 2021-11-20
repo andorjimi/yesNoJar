d93fd508e0a5b9b7b9f97a7fa3796f05350ee064
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-20 17:50:13 +0800

    Ask Button now shows paper when clicked.

---
+++ main.js
@@ -0,0 +1,17 @@
+function buttonClick() {
+	var x = document.getElementById('paper');
+	if (x.style.display == 'block') {
+		x.style.display = 'none';
+	} else {
+		x.style.display = 'block';
+	}
+}
+
+// function buttonClick() {
+// 	var x = document.getElementById('paper');
+// 	if (x.style.visibility == 'visible') {
+// 		x.style.visibility = 'hidden';
+// 	} else {
+// 		x.style.visibility = 'visible';
+// 	}
+// }
