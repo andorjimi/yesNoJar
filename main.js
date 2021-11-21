79f1d302366beb28e4c209d2ec77766757cbe078
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-21 14:51:20 +0800

    Show hide button working

--- main.js
+++ main.js
@@ -1,39 +1,15 @@
-// function buttonClick() {
-// 	var x = document.getElementById('paper');
-// 	if (x.style.display == 'block') {
-// 		x.style.display = 'none';
-// 	} else {
-// 		x.style.display = 'block';
-// 	}
-// }
+const paper = document.querySelector('#paper');
+const answer = document.querySelector('#answer');
 
-// function buttonClick() {
-// 	var x = document.querySectorAll('.answers');
-// 	if (x.style.display == 'block') {
-// 		x.style.display = 'none';
-// 	} else {
-// 		x.style.display = 'block';
-// 	}
-// }
+function showHide(element) {
+	if (element.style.display == 'block') {
+		element.style.display = 'none';
+	} else {
+		element.style.display = 'block';
+	}
+}
 
-// const parent = document.querySelectorAll(".answers")
-
-// parent.forEach(changeColor)
-
-// function changeColor(element) {
-// 	element..style.backgroundColor = "#333"
-// }
-
-// const paper = document.querySelector('.answers')[0];
-// const answer = document.querySelector('.answers')[1];
-
-// buttonClick(paper);
-// buttonClick(answer);
-
-// function buttonClick(element) {
-// 	if (element.style.display == 'block') {
-// 		element.style.display = 'none';
-// 	} else {
-// 		element.style.display = 'block';
-// 	}
-// }
+function buttonClick(element) {
+	showHide(paper);
+	showHide(answer);
+}
