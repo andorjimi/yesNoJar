5f1ccbf26a5d4b75e1294880018c5636c97d2131
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-21 16:16:57 +0800

    Finished. JavaScript =  Working HTML = Working CSS = Working

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const paper = document.querySelector('#paper');
-const answer = document.querySelector('#answer');
+let answer = document.querySelector('#answer');
+
+function yesNo() {
+	let newAnswer = Math.floor(Math.random() * 3);
+
+	switch (newAnswer) {
+		case 0:
+			result = 0;
+			newAnswer = 'No';
+			return newAnswer;
+		case 1:
+			result = 1;
+			newAnswer = 'Yes';
+			return newAnswer;
+		case 2:
+			result = 2;
+			newAnswer = '...';
+			return newAnswer;
+	}
+}
 
 function showHide(element) {
 	if (element.style.display == 'block') {
 		element.style.display = 'none';
 	} else {
-		element.style.display = 'block';
+		if (element == answer) {
+			answer.innerHTML = yesNo();
+			element.style.display = 'block';
+		} else {
+			element.style.display = 'block';
+		}
 	}
 }
 
