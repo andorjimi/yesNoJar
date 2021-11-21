6a0213f55373db62dcbf3e63e1eecd0b37f82410
Jimi <50575362+andorjimi@users.noreply.github.com>
2021-11-21 14:12:29 +0800

    Cleaned up code

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-function buttonClick() {
-	var x = document.getElementById('paper');
-	if (x.style.display == 'block') {
-		x.style.display = 'none';
-	} else {
-		x.style.display = 'block';
-	}
-}
-
 // function buttonClick() {
 // 	var x = document.getElementById('paper');
-// 	if (x.style.visibility == 'visible') {
-// 		x.style.visibility = 'hidden';
+// 	if (x.style.display == 'block') {
+// 		x.style.display = 'none';
 // 	} else {
-// 		x.style.visibility = 'visible';
+// 		x.style.display = 'block';
+// 	}
+// }
+
+// function buttonClick() {
+// 	var x = document.querySectorAll('.answers');
+// 	if (x.style.display == 'block') {
+// 		x.style.display = 'none';
+// 	} else {
+// 		x.style.display = 'block';
+// 	}
+// }
+
+// const parent = document.querySelectorAll(".answers")
+
+// parent.forEach(changeColor)
+
+// function changeColor(element) {
+// 	element..style.backgroundColor = "#333"
+// }
+
+// const paper = document.querySelector('.answers')[0];
+// const answer = document.querySelector('.answers')[1];
+
+// buttonClick(paper);
+// buttonClick(answer);
+
+// function buttonClick(element) {
+// 	if (element.style.display == 'block') {
+// 		element.style.display = 'none';
+// 	} else {
+// 		element.style.display = 'block';
 // 	}
 // }
